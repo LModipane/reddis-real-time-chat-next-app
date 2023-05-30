733a718758dc707812c2490b655da8ad4cbd25b2
shaun <modipanesh@gmail.com>
2023-05-30 09:22:43 +0200

    connected to database

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,7 @@
-export default function Home() {
+import { db } from '@/lib/db';
+
+export default async function Home() {
+	await db.set('message', 'hello world');
 	return (
 		<main className="flex min-h-screen flex-col items-center justify-between p-24">
 			hello world
